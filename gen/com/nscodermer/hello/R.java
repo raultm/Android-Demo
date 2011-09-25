72b486a20d0ee186bdb03961e00a18914eb2bdf3
Raul Tierno <raultm@gmail.com>
2011-09-25 15:16:50 +0200

    Añadido Button y modificado height ImageView

--- gen/com/nscodermer/hello/R.java
+++ gen/com/nscodermer/hello/R.java
@@ -17,13 +17,16 @@ public final class R {
         public static final int rancio_quisquilla=0x7f020003;
     }
     public static final class id {
-        public static final int image=0x7f050000;
+        public static final int action_button=0x7f050002;
+        public static final int bocadillo=0x7f050000;
+        public static final int character_image=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int enter_button=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
