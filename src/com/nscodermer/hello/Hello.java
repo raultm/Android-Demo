f902f98b4a395e642334f5f288bb8001a7f52dfa
Raul Tierno <raultm@gmail.com>
2011-09-25 16:24:36 +0200

    Demo completada

--- src/com/nscodermer/hello/Hello.java
+++ src/com/nscodermer/hello/Hello.java
@@ -1,11 +1,13 @@
 package com.nscodermer.hello;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Hello extends Activity implements OnClickListener{
@@ -21,5 +23,23 @@ public class Hello extends Activity implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		Log.v("Home","Id del Button : "+v.getId());
+		mostrarMensajeConfirmación();
+		mostrarSaludoAntonioRecio();
+		ponerImageAntonioRecio();
+	}
+
+	private void mostrarSaludoAntonioRecio() {
+		TextView bocadillo = (TextView)findViewById(R.id.bocadillo);
+		bocadillo.setText(R.string.mensaje_antonio);
+	}
+
+	private void ponerImageAntonioRecio() {
+		ImageView characterImage = (ImageView)findViewById(R.id.character_image);
+		characterImage.setImageResource(R.drawable.rancio_cigala);
+	}
+
+	private void mostrarMensajeConfirmación() {
+		EditText inputName = (EditText) findViewById(R.id.input_name);
+		Toast.makeText(Hello.this, inputName.getText() + ", ya eres un nuevo vecino.", 5).show();
 	}
 }
---
+++ src/com/nscodermer/hello/Neighbour.java
@@ -0,0 +1,5 @@
+package com.nscodermer.hello;
+
+public class Neighbour {
+
+}
