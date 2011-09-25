9672ca8d0df489204795dffc3e04a06a8ff4dff2
Raul Tierno <raultm@gmail.com>
2011-09-25 15:30:00 +0200

    Main Class implements OnClickListener Ctrl+Shift+O to auto-import

--- src/com/nscodermer/hello/Hello.java
+++ src/com/nscodermer/hello/Hello.java
@@ -2,12 +2,20 @@ package com.nscodermer.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Hello extends Activity {
+public class Hello extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
