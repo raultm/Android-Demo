dfa0991392da0ef56869753165aae46188c7a852
Raul Tierno <raultm@gmail.com>
2011-09-25 15:46:22 +0200

    Set up Listener in Button

--- src/com/nscodermer/hello/Hello.java
+++ src/com/nscodermer/hello/Hello.java
@@ -1,9 +1,12 @@
 package com.nscodermer.hello;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class Hello extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
@@ -11,11 +14,12 @@ public class Hello extends Activity implements OnClickListener{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        View neighbourButton = findViewById(R.id.action_button); 
+        neighbourButton.setOnClickListener(this); 
     }
 
 	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onClick(View v) {
+		Log.v("Home","Id del Button : "+v.getId());
 	}
 }
